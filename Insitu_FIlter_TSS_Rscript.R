d434aef61131a2c76bfdc28232ad282b9d3033b2
Thea <theamarks@gmail.com>
2019-11-08 16:37:52 -0800

    Created average TPM summary table (average of each field day), write .csv to TPM output folder

--- Insitu_FIlter_TSS_Rscript.R
+++ Insitu_FIlter_TSS_Rscript.R
@@ -13,12 +13,16 @@ library(tidyverse)
 library(lubridate)
 
 ######################################################################
-# 
+# Set Up Directory and read in TPM Data
 ######################################################################
 
 TPM_directory = "./TPM_POM_PIM"
 
 TPM_data = fread(file.path(TPM_directory, "Insitu_Filter_POM_PIM_Data.csv"))
+
+######################################################################
+# Summary Table & Average Table per field day
+######################################################################
 
 TPM_summary_table <- TPM_data %>% 
   mutate(TPM = ((dry_weight_corrected_g - filter_weight_corrected_g) * 1000 / sample_volume_ml),
@@ -26,6 +30,13 @@ TPM_summary_table <- TPM_data %>%
          POM = TPM - PIM,
          Organic_Content_Ratio = POM / TPM) %>% 
   as_tibble()
+
+Avg_TPM_summary_table <- TPM_summary_table %>% 
+  group_by(Date, Site, sample_type) %>% 
+  summarize(Avg_TPM = mean(TPM),
+            Avg_PIM = mean(PIM),
+            Avg_POM = mean(POM),
+            Avg_OC_Ratio = mean(Organic_Content_Ratio)) 
 
 ######################################################################
 # Create TPM Output folder
@@ -39,9 +50,10 @@ if(!dir.exists(TPM_output_directory))
 TPM_output_directory
 
 #####################################################################
-# Export TPM Summary Table to project Output folder
+# Export Summary Tables to project Output folder
 ######################################################################
 
 write_csv(TPM_summary_table, file.path(TPM_output_directory, "TPM_summary_table.csv"))
+write_csv(Avg_TPM_summary_table, file.path(TPM_output_directory, "Avg_TPM_summary_table.csv"))
 
           
