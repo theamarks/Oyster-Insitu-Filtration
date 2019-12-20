0e20ebf847ce1a255a0babd383a97f925c360b30
Thea <theamarks@gmail.com>
2019-12-20 12:54:51 -0800

    Rearrange data to graph and pull legend info correctly

--- Test Graph Code for DZ.R
+++ Test Graph Code for DZ.R
@@ -36,16 +36,23 @@ legend_info <- aFile_Mod %>%
   labs(x = ""))
 
 #################### Test function guts - Graph Filtraiton after time adjustment #################
+aTimeSeriesFile <- read_csv("output/4_Filtration_Calculations/Insitu_Filter_NPD_41519.csv")
+
 aFile_Mod = aTimeSeriesFile %<>%
+  dplyr::select(Time, Date, Site, Experiment, Sonde_Up, Chl_ug_L_Up,
+                Sonde_Down, Chl_ug_L_Down, pcnt_Chl_rmvd, L_hr_m2) %>% 
   dplyr::mutate(Time = as.hms(Time),
                 Experiment = ifelse(Experiment %in% c("sbs_after", "sbs_before", "Filtration"),
-                                    Experiment, "Filtration"),
-                Experiment_Title = paste0('aFileName', " - ", Experiment))
+                                    Experiment, "Negative Control"),
+                Experiment_Title = paste0('aFileName', " - ", Experiment)) %>% 
+  gather(Chl_ug_L_Up, Chl_ug_L_Down, key = Position, value = Chl_ug_L)
 
 legend_info <- aFile_Mod %>%
-  dplyr::select(Sonde, Position) %>%
+  dplyr::select(Sonde_Up, Sonde_Down) %>%
   dplyr::distinct() %>%
-  dplyr::mutate(legend = paste0(Position, " ", Sonde)) 
+  gather(Sonde_Up, Sonde_Down, key = Position, value = Sonde) %>% 
+  dplyr::mutate(legend = ifelse(Position == "Sonde_Up", paste0('Up', ' ', Sonde),
+                                ifelse(Position == "Sonde_Down", paste0('Down', ' ', Sonde), NA)))
 
 (one_plot = ggplot(data = aFile_Mod, aes(x = Time, y = Chl_ug_L, group = Position, color = Position)) +
     # ^^^ Changed Sonde to Position ^^^
