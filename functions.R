66ae356d9bd093e710744ebc98a50a93d6cfb962
Thea <theamarks@gmail.com>
2020-02-25 15:25:39 -0800

    changed variable names in CreateSbsDensityPlot to reflect objects present in global environment

--- functions.R
+++ functions.R
@@ -270,7 +270,7 @@ calculateErrorStats = function(aTimeSeriesFile, aFileName, aManualCorrectionsFil
   
   
   # dataframe for graphing & individual sonde stats
-  distrubution = aTimeSeriesFile %>% 
+  adistrubution = aTimeSeriesFile %>% 
       dplyr::filter(Experiment %in% c("sbs_before", "sbs_after")) %>% # all sbs values used in correction
       select(Time, Chl_ug_L, Position, Experiment) %>% 
       tidyr::pivot_wider(names_from = Position, values_from = Chl_ug_L) %>% # spread data to remove NAs
@@ -278,7 +278,7 @@ calculateErrorStats = function(aTimeSeriesFile, aFileName, aManualCorrectionsFil
       pivot_longer(c("Up", "Down"), names_to = "Position", values_to = "Chl_ug_L") # make data longer again for graphs
   
   # downstream summary stats - to populate graphs
-  distrubution_down <- distrubution %>%
+  distrubution_down <- adistrubution %>%
       filter(Position %in% "Down") %>% 
       summarise(mean_chl_down = mean(Chl_ug_L),
                 median_chl_down = median(Chl_ug_L),
@@ -286,7 +286,7 @@ calculateErrorStats = function(aTimeSeriesFile, aFileName, aManualCorrectionsFil
                 se_chl_down = sd_chl_down/sqrt(length(Chl_ug_L)))
   
   # upstream summary stats - to populate graphs
-  distrubution_up <- distrubution %>%
+  distrubution_up <- adistrubution %>%
       filter(Position %in% "Up") %>% 
       summarise(mean_chl_up = mean(Chl_ug_L),
                 median_chl_up = median(Chl_ug_L),
@@ -310,13 +310,14 @@ calculateErrorStats = function(aTimeSeriesFile, aFileName, aManualCorrectionsFil
     Sample_Count = afileSbsStats$Sample_count)
   
   return(Sbs_stat_summary)
+  return(adistrubution)
   
 }
 
 ######################################################################################
 ## This function Graphs Density Plot of SBS Trails 
 ######################################################################################
-createSbsDensityPlot = function(aTimeSeriesFile, aFileName, aGraphOutputDirectory, aType)
+createSbsDensityPlot = function(adistrubution, aFileName)
 {
 
 Sbs_stats_plot = Sbs_stat_summary %>% 
@@ -331,7 +332,7 @@ Sbs_stats_plot <- t(Sbs_stats_plot) # transpose columns and rows
 Sbs_stats_plot_text <- ggpubr::ggtexttable(Sbs_stats_plot, theme = ttheme("blank")) # dataframe to text table
 
 # Frequency polygon of sbs measurements
-(distrubution_plot <- ggplot(data = distrubution, aes(x = Chl_ug_L)) +
+(distrubution_plot <- ggplot(data = adistrubution, aes(x = Chl_ug_L)) +
     geom_freqpoly(aes(color = Position), alpha = 0.6, binwidth = 0.1, size = 1) +
     scale_color_manual(values = c("Down" = wes_palette("Cavalcanti1")[3],
                                   "Up" = wes_palette("Cavalcanti1")[2])) +
